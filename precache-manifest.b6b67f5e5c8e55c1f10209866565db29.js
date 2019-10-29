4cfaf16fdedf664dfbc5a3d0bf5e4b1f72128a0d
jbpkp07 <jeremy.w.barnes@gmail.com>
2019-10-29 03:35:51 -0700

    Updates

---
+++ precache-manifest.b6b67f5e5c8e55c1f10209866565db29.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465b508262f35e9f8bab0355391a5d0f",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "34fa9f0b45ee147290e2",
+    "url": "/ClickyGame/static/js/2.ed3defaf.chunk.js"
+  },
+  {
+    "revision": "c30e7facf4548be51652",
+    "url": "/ClickyGame/static/js/main.3e52a421.chunk.js"
+  },
+  {
+    "revision": "eea99bef7721d0a18d16",
+    "url": "/ClickyGame/static/js/runtime-main.eda1b526.js"
+  }
+]);
